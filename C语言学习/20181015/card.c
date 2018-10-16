9c6f8893e2c74a6356e645b97c619fd7546fdd0e
mazey <mazey@mazey.net>
2018-10-16 10:16:25 +0800

    switch

--- C语言学习/20181015/card.c
+++ C语言学习/20181015/card.c
@@ -6,7 +6,7 @@ int main() {
     puts("输入牌名：");
     scanf("%2s", card_name);
     int val = 0;
-    if (card_name[0] == 'K') {
+    /*if (card_name[0] == 'K') {
         val = 10;
     } else if (card_name[0] == 'Q') {
         val = 10;
@@ -16,6 +16,18 @@ int main() {
         val = 11;
     } else {
         val = atoi(card_name);
+    }*/
+    switch (card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name);
     }
     printf("这张牌的点数是：%i\n", val);
     /* 范围 3 - 6 */
