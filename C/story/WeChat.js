7e1c6dd1941856e6209a5dfdac2fea0a20fe7abd
mazey <mazey@mazey.net>
2018-11-08 23:18:57 +0800

    第一次说早安！

--- C/story/WeChat.js
+++ C/story/WeChat.js
@@ -1,7 +1,18 @@
-setTimeout(
-    () => {
-        angular.element('pre:last').scope().editAreaCtn = `✔️安全的电量\n✔️公交卡\n✔️伞`
-        document.querySelector('.btn_send').click()
-    },
-    3000 // new Date('2018-11-08 21:04:00') - Date.now()
-)
+function Ohayo (date, msg) {
+    const D = new Date()
+    const [YYYY, MM, DD] = [D.getFullYear(), D.getMonth() + 1, D.getDate()]
+    let Fantastic = `${YYYY}-${MM}-${DD} ${date}`
+    if (new Date(Fantastic) - Date.now() < 0) {
+        Fantastic = `${YYYY}-${MM}-${DD + 1} ${date}`
+    }
+    console.log(Fantastic)
+    setTimeout(
+        () => {
+            angular.element('pre:last').scope().editAreaCtn = msg
+            document.querySelector('.btn_send').click()
+        },
+        new Date(Fantastic) - Date.now()
+    )
+}
+
+Ohayo('07:05:00', '早安！\n要乖乖吃药，吃早饭！')
