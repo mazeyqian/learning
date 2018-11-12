d265610b15772998e1820729a0740f702b7e0e10
mazey <mazey@mazey.net>
2018-11-12 11:21:40 +0800

    增加工作消息

--- C/story/WeChat.js
+++ C/story/WeChat.js
@@ -1,3 +1,4 @@
+// 定时发送
 function ohayo (date, msg) {
     const D = new Date()
     const [YYYY, MM, DD] = [D.getFullYear(), D.getMonth() + 1, D.getDate()]
@@ -14,7 +15,7 @@ function ohayo (date, msg) {
         new Date(Fantastic) - Date.now()
     )
 }
-
+// 重复次数 / 以天为单位
 function hebdomad (fn, repeatCount) {
     fn()
     repeatCount--
@@ -26,10 +27,16 @@ function hebdomad (fn, repeatCount) {
         repeatCount--
     }
 }
-
-function kb () {
-    ohayo('07:10:00', '早安！\n要乖乖吃药，吃早饭！')
-    ohayo('07:30:00', '✔️安全的电量\n✔️公交卡\n✔️暖暖的衣服')
+// 多个消息整合
+function pinnacle () {
+    ohayo('11:43:00', '准备吃午饭了！\n午饭！\n饭！')
+    ohayo('18:00:00', '下班！\n下班！！\n下班！！！')
 }
+hebdomad(pinnacle, 5)
 
-hebdomad(kb, 5)
+// function kb () {
+//     ohayo('07:10:00', '早安！\n要乖乖吃药，吃早饭！')
+//     ohayo('07:30:00', '✔️安全的电量\n✔️公交卡\n✔️暖暖的衣服')
+// }
+//
+// hebdomad(kb, 5)
