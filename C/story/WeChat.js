e123b60c8d87bd49eb1852a7a3317d1fa46ff4c3
mazey <mazey@mazey.net>
2018-11-14 23:35:46 +0800

    1115 早安

--- C/story/WeChat.js
+++ C/story/WeChat.js
@@ -62,20 +62,23 @@ function getWeatherByMazey () {
 }
 // getWeatherByMazey()
 // 多个消息整合
-// function pinnacle () {
-//     ohayo('11:43:00', '准备吃午饭了！\n午饭！\n饭！')
-//     ohayo('18:00:00', '下班！\n下班！！\n下班！！！')
-// }
-// hebdomad(pinnacle, 5)
+function pinnacle () {
+    ohayo('7:05:00', 'weatherDes')
+    ohayo('10:40:00', '年轻人总以为自己有的选择，结果还不是点外卖。')
+    ohayo('11:43:00', '准备吃午饭了！\n午饭！\n饭！')
+    ohayo('17:50:00', '各部门注意，马上就要下班了！')
+    ohayo('18:00:00', '下班！\n下班！！\n下班！！！')
+}
+hebdomad(pinnacle, 2)
 // function kb () {
 //     ohayo('07:10:00', '早安！\n要乖乖吃药，吃早饭！')
 //     ohayo('07:30:00', '✔️安全的电量\n✔️公交卡\n✔️暖暖的衣服')
 // }
 //
 // hebdomad(kb, 5)
-function yy () {
-    ohayo('7:00:00', '早安！', getWeatherByMazey)
-    ohayo('7:05:00', '提前看了下今天下雨，不知道上面自动获取+拼接的准不准，总之记得带伞！')
-    ohayo('7:00:30', 'weatherDes')
-}
-hebdomad(yy)
+// function yy () {
+//     ohayo('7:00:00', '早安！', getWeatherByMazey)
+//     ohayo('7:05:00', '提前看了下今天下雨，不知道上面自动获取+拼接的准不准，总之记得带伞！')
+//     ohayo('7:00:30', 'weatherDes')
+// }
+// hebdomad(yy)
