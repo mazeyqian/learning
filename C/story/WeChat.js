9bc02a9dea71d258f60bb2c10a4b65c54780762f
mazey <mazey@mazey.net>
2018-11-15 19:00:52 +0800

    go

--- C/story/WeChat.js
+++ C/story/WeChat.js
@@ -12,13 +12,15 @@ function ohayo (date, msg, fn0) {
             if (fn0) {
                 fn0()
             }
-            if (msg === 'weatherDes') {
-                msg = weatherDes
+            if (msg === 'weatherNow') {
+                msg = weatherNow
+            } else if (msg === 'weatherDaily') {
+                msg = weatherDaily
             }
             angular.element('pre:last').scope().editAreaCtn = msg
             document.querySelector('.btn_send').click()
         },
-        new Date(Fantastic) - Date.now()
+        date === '00:00:00' ? 25 : new Date(Fantastic) - Date.now()
     )
 }
 // 重复次数 / 以天为单位
@@ -34,10 +36,11 @@ function hebdomad (fn, repeatCount = 1) {
     }
 }
 // 天气
-let weatherDes = '不知道怎么回事，今天还没有获取天气！'
+let weatherNow = '不知道怎么回事，今天还没有获取实时天气！'
+let weatherDaily = '不知道怎么回事，今天还没有获取天气！'
 // TODO: /life/suggestion.json
 // 获取天气
-function getWeather (obj) {
+function getWeatherNow (obj) {
     const {text, temperature} = obj.results[0].now
     let msg = ''
     if (parseInt(temperature, 10) <= 13) {
@@ -46,32 +49,43 @@ function getWeather (obj) {
     if (text.includes('雨')) {
         msg += '\n别忘了带伞！'
     }
-    weatherDes = `实时天气：${text}，温度${temperature}°C！${msg}`
-    console.log(weatherDes)
+    weatherNow = `实时天气：${text}，温度${temperature}°C！${msg}`
+    console.log(weatherNow)
+}
+// 获取天气
+function getWeatherDaily (obj) {
+    const {text_day, text_night, high, low, wind_direction, wind_speed} = obj.results[0].daily[0]
+    weatherDaily = `今日天气：白天${text_day}，夜晚${text_night}；\n温度：最低${low}°C，最高${high}°C；\n风向：${wind_direction}，风速：${wind_speed}km/h！`
 }
 // 失败反馈
 function getError (obj) {
-    weatherDes = `不知道怎么回事，获取天气失败了！\n原因：${obj.status}`
-    console.log(weatherDes)
+    weatherDaily = weatherNow = `不知道怎么回事，获取天气失败了！\n原因：${obj.status}`
 }
 // Jsonp 获取天气
 function getWeatherByMazey () {
     const script = document.createElement('script')
-    script.setAttribute('src', 'https://mazey.cn/server/weather')
+    script.setAttribute('src', 'https://mazey.cn/server/weather/now')
+    document.head.appendChild(script)
+}
+function getWeatherDailyByMazey () {
+    const script = document.createElement('script')
+    script.setAttribute('src', 'https://mazey.cn/server/weather/daily')
     document.head.appendChild(script)
 }
 // getWeatherByMazey()
 // 多个消息整合
 function pinnacle () {
-    ohayo('7:00:00', '早上好，今天也是吃不到全家桶的一天呢！', getWeatherByMazey)
-    ohayo('7:05:00', 'weatherDes')
+    ohayo('07:00:00', '早上好，今天也是吃不到全家桶的一天呢！')
+    ohayo('07:00:00', '', getWeatherByMazey)
+    ohayo('07:00:00', '', getWeatherDailyByMazey)
+    ohayo('07:05:00', 'weatherNow')
     ohayo('10:40:00', '年轻人总以为自己有的选择，结果还不是点外卖。')
     ohayo('11:43:00', '准备吃午饭了！\n午饭！\n饭！')
     ohayo('17:50:00', '各部门注意，马上就要下班了！', getWeatherByMazey)
-    ohayo('17:55:00', 'weatherDes')
+    ohayo('17:55:00', 'weatherNow')
     ohayo('18:00:00', '下班！\n下班！！\n下班！！！')
 }
-hebdomad(pinnacle, 2)
+hebdomad(pinnacle, 1)
 // function kb () {
 //     ohayo('07:10:00', '早安！\n要乖乖吃药，吃早饭！')
 //     ohayo('07:30:00', '✔️安全的电量\n✔️公交卡\n✔️暖暖的衣服')
@@ -81,6 +95,10 @@ hebdomad(pinnacle, 2)
 // function yy () {
 //     ohayo('7:00:00', '早安！', getWeatherByMazey)
 //     ohayo('7:05:00', '提前看了下今天下雨，不知道上面自动获取+拼接的准不准，总之记得带伞！')
-//     ohayo('7:00:30', 'weatherDes')
+//     ohayo('7:00:30', 'weatherNow')
 // }
 // hebdomad(yy)
+// ohayo('00:00:00', '', getWeatherDailyByMazey)
+// setTimeout(() => {
+//     ohayo('00:00:00', 'weatherDaily')
+// }, 2000)
