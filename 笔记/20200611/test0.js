60f4e04b19fc876a7570f32436c5e196a335a9e8
钱程 <qiancheng@qutoutiao.net>
2020-06-19 17:14:12 +0800

    feat: 测试Promise

--- 笔记/20200611/test0.js
+++ 笔记/20200611/test0.js
@@ -1,5 +1,36 @@
-async function foo () {
-    return Promise.resolve(123)
+let defer = () => {
+    let pending = []
+    return {
+        resolve: (value) => {
+            if (pending) {
+                pending.forEach(callback => callback(value))
+                pending = undefined
+            }
+        },
+        then: (callback) => {
+            if (pending) {
+                pending.push(callback)
+            } else {
+                callback(value)
+            }
+        }
+    }
 }
 
-foo().then(res => console.log(res))
+function foo () {
+    let d = defer()
+    setTimeout(() => {
+        d.resolve('foo setTimeout')
+    }, 3000)
+    return d
+}
+
+foo().then(res => {
+    console.log(res)
+})
+
+foo().then(res => {
+    console.log('second')
+})
+
+console.log('script end')
