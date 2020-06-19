25a7ad5495587db0f535fa9a63d8b011e3de41a4
钱程 <qiancheng@qutoutiao.net>
2020-06-19 19:00:52 +0800

    feat: nil

--- Go/test3.go
+++ Go/test3.go
@@ -15,6 +15,12 @@ func main () {
 	fmt.Println(TWO)
 	fmt.Println(THREE)
 
-	foo := 'A'
+	foo, bar, baz := 'A', `B`, "haha\\ 123"
 	fmt.Println(foo)
+	fmt.Println(bar)
+	fmt.Println(baz)
+
+	a := 3.14
+	b := int(a)
+	fmt.Println("b", b)
 }
