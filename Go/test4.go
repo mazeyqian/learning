350d16bf035d86d133c2649d234cb16a05ab693e
钱程 <qiancheng@qutoutiao.net>
2020-06-20 23:56:55 +0800

    feat: 默认都只结束当前一层循环，如果想要结束到指定循环，需要给循环体前贴上标签

--- Go/test4.go
+++ Go/test4.go
@@ -11,4 +11,17 @@ func main () {
 	case 4:
 		fmt.Println("没找到！")
 	}
+	haha:
+	for i :=0; i < 100; i++ {
+		fmt.Println("circle", i)
+		if i == 5 {
+			break
+		}
+		for j := 0; j < 7; j++ {
+			if j == 2 {
+				break haha
+			}
+			fmt.Println(i, j)
+		}
+	}
 }
