6cd01a083e2f58e134264b77708ba72ca3008233
社区第六代前端 <mazeyqian@lilith.com>
2021-11-15 11:20:09 +0800

    feat: 添加文章案例 Promise + Async&Await + Array.reduce + 函数递归 解决网络/接口请求的依次/排队不间断间隔访问

---
+++ 笔记/20211115/test.js
@@ -0,0 +1,46 @@
+// 模拟网络请求
+function simulateRequest () {
+  const time = 1000;
+  return new Promise(resolve => setTimeout(() => {
+      resolve();
+      console.log(`模拟请求花费 ${time}ms`);
+  }, time));
+}
+
+// 循环顺序请求
+function cycleRequest () {
+  console.log('新的一轮开始请求');
+  // 间隔 1 秒
+  const arr = new Array(10).fill(undefined);
+  arr.reduce(async (last, curr, index) => {
+    await last;
+    return simulateRequest()
+      .then(() => {
+        if (index + 1 === arr.length) {
+          // 完成一轮后重复
+          cycleRequest();
+        }
+      });
+  }, undefined);
+}
+
+// 启动
+cycleRequest();
+
+/*
+新的一轮开始请求
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+新的一轮开始请求
+模拟请求花费 1000ms
+模拟请求花费 1000ms
+...
+*/
