5b9b344ddaf3b7cea9359c97edff4833fd1f85be
钱程 <mazeyqian@gmail.com>
2022-01-25 18:50:46 +0800

    feat: decode

--- Node/test/index.js
+++ Node/test/index.js
@@ -3,3 +3,8 @@ const b1Str = base64.encodeURI('1#2#3');
 const b2Str = base64.encodeURI(b1Str);
 console.log(`第一次：${b1Str}`);
 console.log(`第二次：${b2Str}`);
+
+// TVRBMU5EVWpOall5TmpRMk9DTTFNREExTnpZ
+const str = 
+console.log(`第一次 decode：${base64.decode('TVRBMU5EVWpOall5TmpRMk9DTTFNREExTnpZ')}`);
+console.log(`第二次 decode：${base64.decode('MTA1NDUjNjYyNjQ2OCM1MDA1NzY')}`);
