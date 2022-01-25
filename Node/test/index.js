fba5cf355db607335588f65530a3b776c5c0ad95
钱程 <mazeyqian@gmail.com>
2022-01-25 14:50:09 +0800

    feat: encodeURI

--- Node/test/index.js
+++ Node/test/index.js
@@ -1,5 +1,5 @@
 const base64 = require('js-base64');
-const b1Str = base64.encode('1#2#3');
-const b2Str = base64.encode(b1Str);
+const b1Str = base64.encodeURI('1#2#3');
+const b2Str = base64.encodeURI(b1Str);
 console.log(`第一次：${b1Str}`);
 console.log(`第二次：${b2Str}`);
