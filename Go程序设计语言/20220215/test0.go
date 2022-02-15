2df8ca5b2f1c897ed4ce939dc3d3b19d12f2a936
钱程 <mazey@mazey.net>
2022-02-15 22:00:09 +0800

    feat: 字符串字面量

---
+++ Go程序设计语言/20220215/test0.go
@@ -0,0 +1,15 @@
+// test
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const GoUsage = `Go is a tool for managing Go source code:
+	
+	Usage:
+		go command [arguments]
+	`
+	fmt.Println(GoUsage)
+}
