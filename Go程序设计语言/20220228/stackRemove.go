a7643f05525a0d91b90e233157e45e9c782a68c4
钱程 <mazey@mazey.net>
2022-02-28 22:52:55 +0800

    feat: remove

---
+++ Go程序设计语言/20220228/stackRemove.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func remove(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+func main() {
+	s := []int{5, 6, 7, 8, 9}
+	fmt.Println(remove(s, 2))
+}
