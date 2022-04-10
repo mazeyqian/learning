878d91043720040e5972c0e694ac4ae0ad427622
钱程 <mazey@mazey.net>
2022-04-10 23:40:10 +0800

    chore: complete Merge Two Sorted Lists

--- Interview/Functorz/LinkedList.js
+++ Interview/Functorz/LinkedList.js
@@ -2,6 +2,7 @@
 合并两个有序链表，返回合并后的有序链表
 输入：1->2->4, 1->3->4
 输出：1->1->2->3->4->4
+https://leetcode.com/problems/merge-two-sorted-lists/solution/
 */
 
 console.log('begin');
@@ -29,6 +30,36 @@ const bb = new LList();
   return curr;
 }, 0);
 bb.display();
+
+const mergeTwoLists = function(l1, l2) {
+  let list = new Node()
+  let head = list
+  
+  while (l1 !== null && l2 !== null) {
+
+  // Select the smallest value from either linked list,
+  // then increment that list forward.
+      if (l1.val < l2.val) {
+          list.next = new Node(l1.val)
+          l1 = l1.next
+      } else {
+          list.next = new Node(l2.val)
+          l2 = l2.next
+      }
+      
+      list = list.next
+  }
+  
+// It's possible that one linked list is shorter than the other so we just
+// add on the remainder of the last linked list. It's already sorted :)
+  if (l1 !== null)
+      list.next = l1
+  if (l2 !== null)
+      list.next = l2
+  
+// return .next because this first element in the linkedlist is empty
+  return head.next
+};
 
 console.log('end');
 
